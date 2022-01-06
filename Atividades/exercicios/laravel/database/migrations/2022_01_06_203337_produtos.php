e59ef08c6ccbcd38cdd550600f687d010a5b403e
FilipeLipe <62721945+FilipeLipe@users.noreply.github.com>
2022-01-06 17:40:56 -0300

    Exercicios de Laravel

--- Atividades/exercicios/laravel/database/migrations/2022_01_06_203337_produtos.php
+++ Atividades/exercicios/laravel/database/migrations/2022_01_06_203337_produtos.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProdutosTable extends Migration
+class Produtos extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateProdutosTable extends Migration
      */
     public function up()
     {
-        Schema::create('produtos', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome',100);
-            $table->string('un',3);
-            $table->timestamps();
-        });
+        $table->id();
+        $table->string('nome',100);
+        $table->string('um',3);
+        $table->timestamp('failed_at')->useCurrent();
     }
 
     /**
@@ -28,6 +26,6 @@ class CreateProdutosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('produtos');
+        //
     }
 }
